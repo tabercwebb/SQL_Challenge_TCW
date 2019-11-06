ad166e990fdbab8976f9c07f92156281977cc615
tabercwebb <52960785+tabercwebb@users.noreply.github.com>
2019-11-06 11:08:50 -0800

    Updated Query file

--- Queries.sql
+++ Queries.sql
@@ -28,12 +28,13 @@ ON dept_manager.emp_no = employees.emp_no;
 
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 
-SELECT 
-FROM
-JOIN
-ON
-JOIN
-ON
+SELECT dept_emp.emp_no, employees.first_name, employees.last_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+ORDER BY dept_emp.emp_no;
 
 -- 5. List all employees whose first name is "Hercules" and last names begin with "B."
 
@@ -44,30 +45,30 @@ AND last_name LIKE 'B%';
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
-SELECT
-FROM
-JOIN
-ON
-JOIN
-ON
-WHERE
+SELECT dept_emp.emp_no, employees.first_name, employees.last_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
-SELECT
-FROM
-JOIN
-ON
-JOIN
-ON
-WHERE
-OR
+SELECT dept_emp.emp_no, employees.first_name, employees.last_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+AND departments.dept_name = 'Development';
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
-SELECT
-COUNT() AS ""
-FROM
-GROUP BY
+SELECT last_name,
+COUNT(last_name) AS "Frequency"
+FROM employees
+GROUP BY last_name
 ORDER BY
-COUNT()
+COUNT(last_name) DESC;
