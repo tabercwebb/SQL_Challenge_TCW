858db0e570f7871dbbaf7c2d161190508adb2ca1
tabercwebb <52960785+tabercwebb@users.noreply.github.com>
2019-11-05 22:53:12 -0800

    Updated Queries and Bonus

--- Queries.sql
+++ Queries.sql
@@ -3,29 +3,32 @@
 
 -- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
 
-SELECT
-FROM
-JOIN
-ON
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.gender, salaries.salary
+FROM employees
+JOIN salaries
+ON employees.emp_no = salaries.emp_no
+ORDER BY employees.emp_no;
 
 -- 2. List employees who were hired in 1986.
 
-SELECT
-FROM
-WHERE
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date
+BETWEEN '1986-01-01' AND '1986-12-31'
+ORDER BY hire_date;
 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
 
-SELECT
-FROM
-JOIN
-ON
-JOIN
-ON
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.first_name, employees.last_name, dept_manager.from_date, dept_manager.to_date
+FROM departments
+JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
 
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 
-SELECT
+SELECT 
 FROM
 JOIN
 ON
@@ -34,10 +37,10 @@ ON
 
 -- 5. List all employees whose first name is "Hercules" and last names begin with "B."
 
-SELECT
-FROM
-WHERE
-AND
+SELECT first_name, last_name
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
